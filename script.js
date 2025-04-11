2bfd5799f3b4d0129b8c92bbf8944a8e8b75b8a9
yancarlo0202 <139280488+yancarlo0202@users.noreply.github.com>
2025-04-11 16:29:48 -0400

    finalizando projeto

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
+import axios from "axios";
+
 const containerVideos = document.querySelector(".videos__container");
 
 async function buscarEMostrarVideos() {
+  const urlVideos = import.meta.env.PROD
+    ? "https://gist.githubusercontent.com/antonio-evaldo/e8a63621b51c883931eb3fa3a3eca990/raw/12f5c46ee6dd00d03c051adadaf341e06452cea0/videos.txt"
+    : "http://localhost:3000/videos";
   try {
-    const busca = await axios.geth("http://localhost:3000/videos");
+    const busca = await axios.get(urlVideos);
     const videos = busca.data;
 
     videos.forEach((video) => {
